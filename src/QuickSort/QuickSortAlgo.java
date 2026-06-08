bcbc2b730fbf0ebd0573371fe96b6550316e42c0
Muskan Kumari <muskan91025kumari@gmail.com>
2026-06-08 23:51:58 +0530

    Added Quick Sort Question

---
+++ src/QuickSort/QuickSortAlgo.java
@@ -0,0 +1,57 @@
+package QuickSort;
+
+public class QuickSortAlgo {
+    public static void swape(int [] arr, int i, int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+
+    }
+    public static int partition(int [] arr,int st, int end){
+        int pivot = arr[st];
+        int count = 0;
+        for (int i=st+1;i<=end;i++){
+            if (arr[i] <= pivot){
+                count++;
+            }
+        }
+        int pivotIdx = st + count;
+        swape(arr, st, pivotIdx);
+        int i=st;
+        int j=end;
+        while (i < pivotIdx && j > pivotIdx){
+            while ( arr[i] <= pivot){
+                i++;
+            }
+            while ( arr[j] >= pivot){
+                j--;
+
+            }
+            if (i<pivotIdx && j>pivotIdx) {
+                swape(arr, i, j);
+                i++;
+                j--;
+            }
+
+
+        }
+        return pivotIdx;
+
+
+    }
+    public static void quickSort(int [] arr , int st, int end){
+        if (st >= end){
+            return;
+        }
+        int pi = partition(arr,st,end);
+        quickSort(arr,st, pi-1);
+        quickSort(arr,pi+1,end);
+    }
+    public static void main(String[] args){
+        int [] arr = {7,13,8,5,10,2,4};
+        quickSort(arr,0,arr.length-1);
+        for (int i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
