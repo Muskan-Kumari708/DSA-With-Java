3f721922dcc1ff1acf2051b401f840605edeb6f8
Muskan Kumari <muskan91025kumari@gmail.com>
2026-06-26 23:45:01 +0530

    Initial Commit

---
+++ src/BinarySearch/Search2DArray.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+
+public class Search2DArray {
+//    for(int i=0;i<n;i++ ){
+//        int st = 0, end = matrix[i].length-1;
+//        while(st<=end){
+//            int mid = st+(end-st)/2;
+//            if(matrix[i][mid] == target){
+//                return true;
+//            }
+//            else if(matrix[i][mid]>target){
+//                end = mid-1;
+//            }
+//            else{
+//                st = mid+1;
+//            }
+//        }
+//    }
+//        return false;
+}
